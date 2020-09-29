fdec146136e77e9a6d08668dec96761b70c558d7
Fernando Mendes <fernando@mendes.codes>
2020-09-30 00:06:48 +0100

    Code review

--- mix.exs
+++ mix.exs
@@ -1,12 +1,15 @@
 defmodule Mix.Tasks.Compile.Hoedown do
   def run(_) do
-    if match? {:win32, _}, :os.type do
-      {result, _error_code} = System.cmd("nmake", ["/F", "Makefile.win", "priv\\markdown.dll"], stderr_to_stdout: true)
-      IO.binwrite result
+    if match?({:win32, _}, :os.type()) do
+      {result, _error_code} =
+        System.cmd("nmake", ["/F", "Makefile.win", "priv\\markdown.dll"], stderr_to_stdout: true)
+
+      IO.binwrite(result)
     else
       {result, _error_code} = System.cmd("make", ["priv/markdown.so"], stderr_to_stdout: true)
-      IO.binwrite result
+      IO.binwrite(result)
     end
+
     :ok
   end
 end
@@ -17,11 +20,13 @@ defmodule Markdown.Mixfile do
   @version "0.1.0"
 
   def project do
-    [app: :markdown,
-     version: @version,
-     elixir: ">= 0.14.3 and < 2.0.0",
-     compilers: [:hoedown, :elixir, :app],
-     deps: deps()]
+    [
+      app: :markdown,
+      version: @version,
+      elixir: ">= 0.14.3 and < 2.0.0",
+      compilers: [:hoedown, :elixir, :app],
+      deps: deps()
+    ]
   end
 
   def application do
